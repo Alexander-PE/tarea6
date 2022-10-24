5f0b9ac064430069c29b9ad53caa20d816b5e4b7
Alexander Perez <alexander.pereze02@gmail.com>
2022-10-24 15:07:30 -0400

    Update Genero v2

--- lib/pages/Genero.dart
+++ lib/pages/Genero.dart
@@ -1,5 +1,7 @@
 // ignore: file_names
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http; 
 
 class Genero extends StatefulWidget {
   const Genero({super.key});
@@ -9,14 +11,59 @@ class Genero extends StatefulWidget {
 }
 
 class _GeneroState extends State<Genero> {
+
+  final _textController = TextEditingController();
+
+  List persona = [];
+
+  String name = "";
+  static const man = TextStyle(color: Colors.blue);
+  static const woman = TextStyle(color: Colors.pink);
+
+
+  Future<void> _fetchData(String nombre) async {
+    String apiUrl = 'https://api.genderize.io/?name=$nombre';
+
+    final response = await http.get(Uri.parse(apiUrl));
+    final data = json.decode(response.body);
+
+    setState(() {
+      persona = data;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: const [
-        TextField(
-          decoration: InputDecoration(hintText: 'Inserte un nombre' ,border: OutlineInputBorder()),)
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+
+          Expanded(child: Container(
+            child: Center(
+              child: Text(name, style: const TextStyle(fontSize: 50),)
+            ),
+          )),
+
+          TextField(
+            controller: _textController,
+            decoration: const InputDecoration(hintText: 'Inserte un nombre' ,border: OutlineInputBorder())
+          ),
+          MaterialButton(
+            onPressed: () {
+              setState(() {
+                name = _textController.text;
+                _fetchData(name);
+              });
+            },
+            color: Colors.blue,
+            child: const Text("Identificar", style: TextStyle(color: Colors.white),),
+          )
+        ],
+      ),
     );
   }
 }
