8a1659e1be510e714f8784da5bd1b32b4ae7bff8
Alexander Perez <alexander.pereze02@gmail.com>
2022-10-24 14:36:23 -0400

    First commit

---
+++ lib/pages/Genero.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: const [
+        TextField(
+          decoration: InputDecoration(hintText: 'Inserte un nombre' ,border: OutlineInputBorder()),)
+      ],
+    );
+  }
+}
