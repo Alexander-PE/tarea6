7d3b649086aef92397da44de72afeb065800dd58
Alexander Perez <alexander.pereze02@gmail.com>
2022-10-24 15:07:18 -0400

    Second

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tarea6/pages/Genero.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,6 +16,7 @@ class _MyAppState extends State<MyApp> {
   
   int page = 0;
   List paginas = const [
+    Genero()
   ];
 
   @override
